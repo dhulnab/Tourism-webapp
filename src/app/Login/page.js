e280f07bd8f6f489beada90833de2a8ea07fcf84
Dhulfiqar Ali <dhulfiqarali6@gmail.com>
2024-01-13 23:51:09 +0300

    2nd commit

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -4,30 +4,33 @@ import styles from "./page.module.css";
 import Input from "@/Components/Input/Input";
 import Button from "@/Components/Button/Button";
 import Image from "next/image";
-import img from "../../../public/undraw_aircraft_re_m05i.svg"
+import img from "../../../public/undraw_aircraft_re_m05i.svg";
 
 const page = () => {
   return (
     <main className={styles.main}>
-    <Container>
-      <h3>تسجيل الدخول</h3>
-      <p>مرحبًا! سجل دخول واستمتع بتجربة فريدة <br/>
-         ادخل الآن لاستكشاف عوالم جديدة وتجارب مثيرة,</p>
-    </Container>
-    <div className={styles.image}>
-      <Image 
-      src={img}
-      alt={"img"}
-      fill
-      />
-    </div>
+      <Container>
+        <h3>تسجيل الدخول</h3>
+        <p>
+          مرحبًا! سجل دخول واستمتع بتجربة فريدة <br />
+          ادخل الآن لاستكشاف عوالم جديدة وتجارب مثيرة,
+        </p>
+      </Container>
+      <div className={styles.image}>
+        <Image src={img} alt={"img"} fill />
+      </div>
       <Container>
         <div className={styles.content}>
-          <Input placeholder={"+964"} style={{ width: "20%", margin: "8px" }} />
-          <Input
-            placeholder={"رقم الهاتف"}
-            style={{ width: "60%", margin: "8px" }}
-          />
+          <div className={styles.num}>
+            <Input
+              placeholder={"+964"}
+              style={{ width: "20%", margin: "8px" }}
+            />
+            <Input
+              placeholder={"رقم الهاتف"}
+              style={{ width: "60%", margin: "8px" }}
+            />
+          </div>
           <Input
             type={"password"}
             placeholder={"كلمة السر"}
