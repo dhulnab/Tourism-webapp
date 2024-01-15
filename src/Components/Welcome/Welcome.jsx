d4ffdd05fb64a57ee6567ca9e7cb0ee50c153761
Dhulfiqar Ali <dhulfiqarali6@gmail.com>
2024-01-15 10:16:25 +0300

    3rd commit

--- src/Components/Welcome/Welcome.jsx
+++ src/Components/Welcome/Welcome.jsx
@@ -2,16 +2,17 @@ import Image from "next/image";
 import styles from "./Welcome.module.css";
 import img from "../../../public/undraw_stranded_traveler_pdbw.svg";
 
-function Welcome() {
+const Welcome = () => {
   return (
-    <div >
+    <div>
       <div className={styles.content}>
         <h3>يا هلا</h3>
         <p>
-          مرحباً بك في تطبيق رحلات <br/>المكان الذي يمكّنك من اكتشاف جمال العراق وما
-          وراءه <br/>انضم إلينا لتصبح جزءًا من مجتمع يربط بين المسافرين وشركات
-          السياحة<br/> اختر رحلتك، احجز بسهولة، وشارك تجاربك، لنبني سويًا رحلات
-          تمتلك طابعًا ثقافيًا استثنائيًا
+          مرحباً بك في تطبيق رحلات <br />
+          المكان الذي يمكّنك من اكتشاف جمال العراق وما وراءه <br />
+          انضم إلينا لتصبح جزءًا من مجتمع يربط بين المسافرين وشركات السياحة
+          <br /> اختر رحلتك، احجز بسهولة، وشارك تجاربك، لنبني سويًا رحلات تمتلك
+          طابعًا ثقافيًا استثنائيًا
         </p>
         <div className={styles.img}>
           <Image src={img} alt={"none"} fill />
@@ -19,6 +20,6 @@ function Welcome() {
       </div>
     </div>
   );
-}
+};
 
 export default Welcome;
